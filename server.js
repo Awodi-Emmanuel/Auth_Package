5fe16a4bd2f31d3c6d882d62e4da7ae675bfb867
emmy70 <emmanuelusman701@gmail.com>
2021-05-12 23:51:35 +0100

    connectDB successful

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const connectDB = require("./db");
+require("dotenv").config(); //allows us to use enviromentatl variable in .env
+
+const { PORT } = process.env;
+
 const app = express();
 
-app.listen(4000, () => {
-  console.log("Listening on port 4000");
+connectDB();
+
+/**
+ * All Middlewares used
+ * in the appication
+ */
+// initialise express middleware
+app.use(express.json({ extended: false }));
+
+// Basic Route - home
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome Home!!!" });
+});
+
+// Server Port
+const port = process.env.PORT || PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
